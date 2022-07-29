5254d4988ad156c096d8161fbfa2e70f359bb08a
Shruti <shrutiswift@gmail.com>
2022-07-29 14:56:10 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-setInterval(myTimer, 1000);
+//setinterval calls a function at a specified intervals
+setInterval(myTimer, 1000); //1000 miliseconds  = 1 second
 function myTimer() {
     const date = new Date();
     document.getElementById("demo").innerHTML = date.toLocaleTimeString();
