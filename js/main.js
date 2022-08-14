01904bc2b5a68bedebb78ef2b3a3cb212d8eb16a
Shruti <shrutiswift@gmail.com>
2022-08-14 21:52:24 +0530

    main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 setInterval(myTimer, 1000);
 function myTimer() {
-    const date = new Date();
+    const date = new Date(); 
     document.getElementById("demo").innerHTML = date.toLocaleTimeString();
 }
 
